1f9475f722df9f36c518532c0f3125f0d8cb8647
kureharyosuke <ohhayo.com@gmail.com>
2021-04-29 03:04:03 +0900

    add: Navbar, People, Planets

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,19 @@
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import Planets from "./components/Planets";
+import People from "./components/People";
+
 function App() {
-  return <div className="App"></div>;
+  const [page, setPage] = useState("planets");
+  return (
+    <div className="App">
+      <h1>Star Wars Info</h1>
+      <Navbar setPage={setPage} />
+      <div className="content">
+        {page === "planets" ? <Planets /> : <People />}
+      </div>
+    </div>
+  );
 }
 
 export default App;
