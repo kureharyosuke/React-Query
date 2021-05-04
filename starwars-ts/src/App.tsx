3df04fd08f106de6c3809e6be070c48405897a0d
kureharyosuke <ohhayo.com@gmail.com>
2021-05-05 01:51:16 +0900

    TypeScript setting

---
+++ starwars-ts/src/App.tsx
@@ -0,0 +1,15 @@
+import * as React, { useState } from "react";
+
+function App() {
+  // const [page, setPage] = useState("planets");
+
+  return (
+    <div className="App">
+      <h1>Star Wars Info</h1>
+      <div className="content">
+      </div>
+    </div>
+  );
+}
+
+export default App;
